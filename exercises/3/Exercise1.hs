9d67a13ae720b3ba5e9607189d7e51b089c0a8e8
Niklas Gruhn <niklas.gruhn@gmail.com>
2019-05-10 18:22:03 +0200

    Collatz is not defined for k=0

--- exercises/3/Exercise1.hs
+++ exercises/3/Exercise1.hs
@@ -1,13 +1,7 @@
 
 -- 1a)
-
--- QUESTION:
--- Should the first entry of the result list be the case k=0 ?
--- Not sure if the exercise implies that. That's means however,
---    collatz 1 = [1, 4, 2, 1, ...]
--- So the smallest k in this case would actually be k=0.
 collatz :: Int -> [Int]
-collatz n = iterate f (f n)
+collatz n = tail (iterate f n)
   where
     f n | even n = n `div` 2
         | odd  n = 3*n + 1
